718f7b48dd42010ab44b77359069f2661e3f2572
Fan-code04 <74674330+Fan-code04@users.noreply.github.com>
2021-02-16 01:32:40 +0100

    Add files via upload

---
+++ css/js.js
@@ -0,0 +1,9 @@
+var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+var toastList = toastElList.map(function (toastEl) {
+  return new bootstrap.Toast(toastEl, option)
+})
+
+var myToastEl = document.getElementById('myToast')
+myToastEl.addEventListener('hidden.bs.toast', function () {
+  // do something...
+})
